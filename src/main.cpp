4ba3ec47a2719a186daac6d83fa05f747892232b
Inorien <Inorien@live.com>
2023-02-22 22:27:13 +0900

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "mob/player.h"
+#include "item/sword.h"
 
 int main() {
 
@@ -10,6 +11,13 @@ int main() {
   Mob::Player player(name);
 
   std::cout << "Created a player with name: " << player.getInfo().getName() << "\n";
+
+  std::cout << player.getInventory();
+
+  auto& inv {player.getInventory()};
+  inv.add(std::make_unique<Item::Weapon::Sword>());
+
+  std::cout << player.getInventory();
 
   return 0;
 
